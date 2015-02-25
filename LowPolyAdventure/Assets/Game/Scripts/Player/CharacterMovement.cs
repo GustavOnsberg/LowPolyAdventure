7f4b1f0f94d688308e31a9e9eecfcb2a27442fdc
GustavOnsberg <onsberg04@gmail.com>
2015-02-25 21:06:43 +0100

    player updated

---
+++ LowPolyAdventure/Assets/Game/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float speed = 5;
+
+
+
+    Ray mouseRay;
+    RaycastHit mouseHit;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+        rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed,
+                                      rigidbody.velocity.y, Input.GetAxis("Vertical") * speed);
+
+
+
+        SetPlayerDirection();
+	}
+
+
+
+
+    void SetPlayerDirection() {
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(mouseRay,out mouseHit, 100))
+        {
+            transform.rotation = Quaternion.LookRotation((new Vector3(mouseHit.point.x, transform.position.y, mouseHit.point.z) - transform.position).normalized);
+            Debug.DrawLine(mouseRay.origin, mouseHit.point + new Vector3(0, 1, 0), Color.blue);
+            Debug.DrawLine(transform.position + new Vector3(0, 1, 0), mouseHit.point + new Vector3(0, 1, 0), Color.red);
+        }
+    }
+}
