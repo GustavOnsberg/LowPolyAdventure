cb65785b020284cb2d01ddf3196f4959b75f24b8
GustavOnsberg <onsberg04@gmail.com>
2015-02-25 10:40:58 +0100

    Added nav mesh - Added camera movement

---
+++ LowPolyAdventure/Assets/Game/Camera/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+
+
+    public float delay;
+
+
+
+    [HideInInspector]
+    public Vector3 goTo;
+
+
+
+
+	void Start () {
+	    
+	}
+	
+	
+
+
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, goTo, delay * Time.deltaTime);
+	}
+}
---
+++ LowPolyAdventure/Assets/Game/Characters/Character Defaults/CharacterToWorld.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterToWorld : MonoBehaviour {
+
+
+
+    GameObject cameraController;
+    CameraController cameraControllerScript;
+
+
+	void Start () {
+        cameraController = GameObject.Find("CameraController");
+        cameraControllerScript = cameraController.GetComponent<CameraController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cameraControllerScript.goTo = transform.position;
+	}
+}
