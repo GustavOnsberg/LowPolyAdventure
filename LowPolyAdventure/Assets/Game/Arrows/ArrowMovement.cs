9e058f1fe5460e9c79ba59044eb0582e80a53fb3
GustavOnsberg <onsberg04@gmail.com>
2015-02-26 01:51:24 +0100

    arrow started

---
+++ LowPolyAdventure/Assets/Game/Arrows/ArrowMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMovement : MonoBehaviour {
+
+	// Use this for initialization
+
+    float speed;
+
+
+	void Start () {
+        speed = GameObject.Find("GameController").GetComponent<GameController>().arrowSpeed;
+        rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
