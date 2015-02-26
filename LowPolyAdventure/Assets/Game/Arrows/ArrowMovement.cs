7fe822aaef3a91255e1668ed447d72d2c1c52816
GustavOnsberg <onsberg04@gmail.com>
2015-02-26 12:45:51 +0100

    random stuff

--- LowPolyAdventure/Assets/Game/Arrows/ArrowMovement.cs
+++ LowPolyAdventure/Assets/Game/Arrows/ArrowMovement.cs
@@ -6,15 +6,20 @@ public class ArrowMovement : MonoBehaviour {
 	// Use this for initialization
 
     float speed;
-
+    bool firstFrame;
 
 	void Start () {
         speed = GameObject.Find("GameController").GetComponent<GameController>().arrowSpeed;
-        rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
+        //rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
+        rigidbody.velocity = Vector3.forward * speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        if (firstFrame)
+        {
+            //rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
+            firstFrame = false;
+        }
 	}
 }
