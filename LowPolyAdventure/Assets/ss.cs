e2d9cfb92392c1652f5bd87cfa378bf65d96127f
GustavOnsberg <onsberg04@gmail.com>
2015-02-27 02:48:57 +0100

    aa

---
+++ LowPolyAdventure/Assets/ss.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ss : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.renderer.material.color = new Color(gameObject.renderer.material.color.r, gameObject.renderer.material.color.g, gameObject.renderer.material.color.b, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
